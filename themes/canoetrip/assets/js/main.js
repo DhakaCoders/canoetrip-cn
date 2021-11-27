b2c2cc80a03281e0f2452e71e00aa7847b16a7b6
kashob000 <skkashob000@gmail.com>
2021-11-27 21:20:55 +0600

    ..

--- themes/canoetrip/assets/js/main.js
+++ themes/canoetrip/assets/js/main.js
@@ -283,21 +283,21 @@ $('.cntrp-faq-accordion-hdr').on('click', function(){
 
 
 /* start of Kashob*/
-if (windowWidth <= 767){
-  if( $('.BtmGallerySlider').length ){
-    $('.BtmGallerySlider').slick({
-      dots: false,
-      arrows: false,
-      infinite: false,
-      autoplay: true,
-      autoplaySpeed: 4000,
-      speed: 700,
-      slidesToShow: 2,
-      slidesToScroll: 1,
+// if (windowWidth <= 767){
+//   if( $('.BtmGallerySlider').length ){
+//     $('.BtmGallerySlider').slick({
+//       dots: false,
+//       arrows: false,
+//       infinite: false,
+//       autoplay: true,
+//       autoplaySpeed: 4000,
+//       speed: 700,
+//       slidesToShow: 2,
+//       slidesToScroll: 1,
 
-    });
-  }
-}
+//     });
+//   }
+// }
 
 
 /* start of Momin */
